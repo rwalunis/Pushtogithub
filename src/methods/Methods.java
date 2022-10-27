bd84fd97f5d0f9b274272e029b5e826b99744894
Rachael W <94757293+rwalunis@users.noreply.github.com>
2022-10-27 19:49:34 -0400

    Add files via upload

---
+++ src/methods/Methods.java
@@ -0,0 +1,18 @@
+package methods;
+
+public class Methods {
+	
+	public static String fullName(String name) {
+		return name;
+	}
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println(fullName("Rachael"));		
+	String nammes = "Joey";
+	int[] arr = {1, 2};
+	System.out.println(arr.length);
+	
+	}	
+}
+
+
